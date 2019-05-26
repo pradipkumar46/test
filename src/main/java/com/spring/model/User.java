17a42eb18711c34f801b128fe5c261333cd54193
pradipkumar <pradipkumar.java@gmail.com>
2019-05-26 18:09:36 +0530

    add User.jasva

---
+++ src/main/java/com/spring/model/User.java
@@ -0,0 +1,26 @@
+package com.spring.model;
+
+public class User {
+
+    private String name;
+    private String address;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
